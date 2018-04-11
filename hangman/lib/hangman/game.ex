f14fcfdeddb3ca4b2cb6db80bbdb5a639307a137
3vangelos <evangelos@posteo.eu>
2018-04-11 08:52:16 +0100

    minor refactoring

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -12,7 +12,9 @@ defmodule Hangman.Game do
   end
 
   def new_game() do
-    new_game(Dictionary.random_word)
+    Dictionary.start()
+    |> Dictionary.random_word()
+    |> new_game()
   end
 
   def move(game, guess) do
